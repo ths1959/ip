2c0c814915240870546ddad3abb11a3fb8730986
ths1959 <ths2891503@gmail.com>
2026-01-18 09:38:42 +0800

    Add support for storing and listing tasks

--- src/main/java/MemoMax.java
+++ src/main/java/MemoMax.java
@@ -9,13 +9,25 @@ public class MemoMax {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm MemoMax\nWhat can I do for you?" + logo);
 
+        String[] tasks = new String[100]; // Task storage
+        int taskCounter = 0; // Serial number for tasks
+        Scanner scanner = new Scanner(System.in);
         while(true) {
-            Scanner scanner = new Scanner(System.in);
             String userInput = scanner.nextLine();
             if (userInput.equals("bye")) { // Exit condition
                 break;
-            } else {
-                System.out.println(userInput); // Echos user inputted commands
+            } else if (userInput.equals("list")) { // List tasks
+                if (taskCounter == 0) { // No task in storage
+                    System.out.println("There are currently no tasks in your list");
+                } else { // Print out all tasks
+                    for (int i = 0; i < taskCounter; i++) {
+                        System.out.println(i+1 + ". " + tasks[i]);
+                    }
+                }
+            } else { // Add task to storage
+                tasks[taskCounter] = userInput;
+                System.out.println("added: " + userInput);
+                taskCounter += 1;
             }
         }
 
