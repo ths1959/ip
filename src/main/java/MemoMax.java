46a8ebd393692ad1bab345875a250f376ec05916
ths1959 <ths2891503@gmail.com>
2026-01-18 08:18:51 +0800

    Add echo functionality and exit condition

--- src/main/java/MemoMax.java
+++ src/main/java/MemoMax.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MemoMax {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,6 +8,17 @@ public class MemoMax {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm MemoMax\nWhat can I do for you?" + logo);
+
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) { // Exit condition
+                break;
+            } else {
+                System.out.println(userInput); // Echos user inputted commands
+            }
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
